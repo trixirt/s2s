4d2c48382f6fd16c34ec9833617fe399a31b23be
trix <tom.rix@sony.com>
2019-02-22 08:28:13 -0800

    A filter that passes files with cpp extension

---
+++ lua/cpp-filter.lua
@@ -0,0 +1,19 @@
+--===----------------------------------------------------------------------===
+--
+--                     The LLVM Compiler Infrastructure
+--
+-- This file is distributed under the University of Illinois Open Source
+-- License. See LICENSE.TXT for details.
+--
+-- Copyright Tom Rix 2019, all rights reserved.
+-- 
+--===----------------------------------------------------------------------===
+function FilterDBEntry(CommandLine, InputFile, InputDirectory, Exe)
+  local r = 0
+  local ext = string.match(InputFile, '[^.]+$')
+  print (ext)
+  if ext == "cpp" then
+     r = 1
+  end
+  return r
+end
