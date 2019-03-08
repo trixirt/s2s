7b59381b9ba35890a73d8f3769ea27c7582679d6
trix <tom.rix@sony.com>
2019-03-08 15:31:34 -0800

    Add a placeholder to look for a certain file in the filter

--- lua/noop-filter.lua
+++ lua/noop-filter.lua
@@ -1,4 +1,12 @@
 function FilterDBEntry(CommandLine, InputFile, InputDirectory, Exe)
   local r = 1
+  -- Customize filter here
+  --
+  -- To look at a single file, uncomment
+  -- local f = ""
+  -- r = 0
+  -- if InputFile == f then
+  --  r = 1
+  -- end
   return r
 end
